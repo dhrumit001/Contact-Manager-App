a9f547e696938d151e8b6a24cb3c7f26b9e74759
dhrumit <dhrumit.patel@ics-global.in>
2024-06-27 10:29:00 +0530

    Add Initial web project for .Net 8 and folder structure

---
+++ Presentation/App.Web/Startup.cs
@@ -0,0 +1,47 @@
+﻿
+
+namespace LearnProject.Web
+{
+
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Contact}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
+
