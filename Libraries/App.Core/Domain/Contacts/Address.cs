b4b67695441affc18339cff89dd7d3867482d8de
dhrumit <dhrumit.patel@ics-global.in>
2024-06-27 10:33:14 +0530

    Added App.Core layer (Library projecrt in .Net 8)

---
+++ Libraries/App.Core/Domain/Contacts/Address.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace App.Core.Domain.Contacts
+{
+    [Table(nameof(Address))]
+    public class Address : BaseEntity
+    {
+        #region Fields
+
+        /// <summary>
+        /// Gets or sets the contact id for address
+        /// </summary>
+        public int ContactId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the country name
+        /// </summary>
+        public string Country { get; set; }
+
+        /// <summary>
+        /// Gets or sets the state name
+        /// </summary>
+
+        public string State { get; set; }
+
+        /// <summary>
+        /// Gets or sets the city name
+        /// </summary>
+        public string City { get; set; }
+
+        /// <summary>
+        /// Gets or sets the street name
+        /// </summary>
+        public string Street { get; set; }
+
+        /// <summary>
+        /// Gets or sets the zip postal code
+        /// </summary>
+        public string ZipPostalCode { get; set; }
+
+        #endregion
+
+        #region Navigation Properties
+
+        /// <summary>
+        /// Gets the contact of address
+        /// </summary>
+        public virtual Contact Contact { get; }
+
+        #endregion
+    }
+}
---
+++ Libraries/App.Core/Domain/Contacts/Contact.cs
@@ -0,0 +1,52 @@
+﻿
+
+namespace App.Core.Domain.Contacts
+{
+    public class Contact : BaseEntity
+    {
+        #region Fields
+
+        /// <summary>
+        /// Gets or sets the name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the phone number
+        /// </summary>
+
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email address
+        /// </summary>
+        public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time of entity creation
+        /// </summary>
+        public DateTime CreatedOnUtc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time of last update of entity
+        /// </summary>
+        public DateTime? UpdatedOnUtc { get; set; }
+
+        #endregion
+
+        #region Navigation Properties
+
+        private IList<Address>? _contactAddresses;
+
+        /// <summary>
+        /// Gets all addresses of contact
+        /// </summary>
+        public virtual IList<Address> ContactAddresses
+        {
+            get => _contactAddresses ?? new List<Address>();
+        }
+
+        #endregion
+
+    }
+}
